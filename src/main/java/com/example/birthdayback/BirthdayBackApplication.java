c5240071ebe8c7fc669c83fab2755e0f07bf9250
Gio <georgesmessanges@hotmail.com>
2022-08-28 17:45:34 +0200

    first commit

---
+++ src/main/java/com/example/birthdayback/BirthdayBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.birthdayback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirthdayBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BirthdayBackApplication.class, args);
+    }
+
+}
